7b343e0e77e1225d3e2ece4666ac64518a878baa
oliverloza <oliverbree@gmail.com>
2023-09-03 13:19:58 -0700

    skeleton

---
+++ group-project-react/src/MakeUpList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import MakeUpCard from "./MakeUpCard"
+function MakeUpList() {
+  return 
+  <div>
+  
+  <ul className="cards">
+
+<MakeUpList/>
+
+
+  </ul>;
+
+
+  </div>
+
+
+
+}
+ 
+
+export default MakeUpList;
---
+++ group-project-react/src/Search.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Search () {
+    return (
+
+        <div className="searchbar">
+            <label htmlFor="search">Search MakeUp:</label>
+            <input 
+            value=""
+    />
+
+        </div>
+    )
+} 
+
+export default Search;
