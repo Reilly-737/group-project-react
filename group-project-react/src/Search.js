b9df30464ddb117aa0e6e8fd955e66ba10a6fdd9
Molly Todd <mollyjt89@gmail.com>
2023-09-07 11:04:00 -0700

    search button added so it's not searching api based on text input anymore

--- group-project-react/src/Search.js
+++ group-project-react/src/Search.js
@@ -10,48 +10,50 @@ function SearchBar() {
     event.target.src = defaultImageUrl;
   };
 
-  useEffect(() => {
-    // Define the fetch function
-
-    async function fetchData() {
-      if (searchText.trim() === "") {
-        // If the search text is empty, clear the results
-        setSearchResults([]);
-        setLoading(false); // Set loading to false when clearing results
-        return;
-      }
-
-      setLoading(true);
-
-      try {
-        // Replace this with your actual API endpoint
-        const response = await fetch(
-          `http://makeup-api.herokuapp.com/api/v1/products.json?q=${encodeURIComponent(
-            searchText
-          )}`
-        );
-
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-
-        const data = await response.json();
-
-        // Filter the data based on the search query
-        const filteredData = data.filter((item) =>
-          item.name.toLowerCase().includes(searchText.toLowerCase())
-        );
-
-        setSearchResults(filteredData);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        setLoading(false);
-      }
+  // Define the fetchData function
+  const fetchData = async () => {
+    if (searchText.trim() === "") {
+      setSearchResults([]);
+      setLoading(false);
+      return;
     }
 
-    fetchData(); // Call the fetchData function when searchText changes
-  }, [searchText]);
+    setLoading(true);
+
+    try {
+      const response = await fetch(
+        `http://makeup-api.herokuapp.com/api/v1/products.json?q=${encodeURIComponent(
+          searchText
+        )}`
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+
+      const filteredData = data.filter((item) =>
+        item.name.toLowerCase().includes(searchText.toLowerCase())
+      );
+
+      setSearchResults(filteredData);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setLoading(false);
+    }
+  };
+
+  const handleSearchClick = () => {
+    // When the "Search" button is clicked, initiate the search
+    fetchData();
+  };
+
+  useEffect(() => {
+    // No changes here; this effect will still run when the component mounts
+    fetchData();
+  }, []);
 
   const handleInputChange = (e) => {
     const inputText = e.target.value;
@@ -67,6 +69,7 @@ function SearchBar() {
         value={searchText}
         onChange={handleInputChange}
       />
+      <button onClick={handleSearchClick}>Search</button>
       <ul>
         {loading ? (
           <li>Loading...</li>
@@ -74,14 +77,12 @@ function SearchBar() {
           searchResults.map((result, index) => (
             <li key={index}>
               <div className="makeup">
-          
                 {result.name}
                 <img
                   src={result.image_link}
                   alt={result.name}
                   onError={handleImageError}
                 />
-         
               </div>
             </li>
           ))
